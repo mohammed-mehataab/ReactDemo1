9c3d80d4a0e7e83fa7ed99d6189574157cc666a7
Mohammed Mehataab <mohammed.mehataab@tigeranalytics.local>
2022-05-18 13:17:34 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 //import logo from './logo.svg';
-import './App.css';
-import profile from './components/profile';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import profile from "./components/profile";
 import home from './components/home';
 import signup from './components/signup';
 import login from './components/login';
+import './App.css';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 function App() {
   return (
+    <>
    <BrowserRouter>
    <Routes>
-    <Route path="/" element={<home/>}></Route>
-    <Route path="/login" element={<login/>}></Route>
-    <Route path="/signup" element={<signup/>}></Route>
-    <Route path="/posts" element={<posts/>}></Route>
-    <Route path="/profile" element={<profile/>}></Route>
+     
+    <Route path="/" element={<home />}></Route>
+    <Route path="/login" element={<login />}></Route>
+    <Route path="/signup" element={<signup />}></Route>
+    <Route path="/posts" element={<posts />}></Route>
+    <Route path="/profile" element={<profile />}></Route>
     </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
