20d9492aa64c109207317ce040216ae098c2942f
Mohammed Mehataab <mohammed.mehataab@tigeranalytics.local>
2022-05-18 12:57:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
    <BrowserRouter>
+   <Routes>
     <Route path="/login" element={<login/>}></Route>
     <Route path="/signup" element={<signup/>}></Route>
     <Route path="/posts" element={<posts/>}></Route>
     <Route path="/profile" element={<profile/>}></Route>
+    </Routes>
     </BrowserRouter>
   );
 }
